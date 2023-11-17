8babe07b0da8b738ddd99fdf228e66ac232431dd
igor.kudlo <igoreklida@gmail.com>
2023-11-17 22:47:36 +0300

    USEPOPCORN: abort fetch request in useEffect

--- 07-usepopcorn/starter/src/App.js
+++ 07-usepopcorn/starter/src/App.js
@@ -78,11 +78,14 @@ export default function App() {
     }
 
     useEffect(() => {
+        const controller = new AbortController();
+
         async function fetchMovies() {
             try {
                 setIsLoading(true);
                 setError("");
-                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+                const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                    { signal: controller.signal });
 
                 if (!res.ok) throw new Error("Something went wrong with fetching movies");
 
@@ -90,8 +93,11 @@ export default function App() {
                 if (data.Response === "False") throw new Error("Movie not found");
 
                 setMovies(data.Search);
+                setError("");
             } catch (err) {
-                setError(err.message);
+                if (err.message !== "AbortError") {
+                    setError(err.message);
+                }
             } finally {
                 setIsLoading(false);
             }
@@ -104,6 +110,10 @@ export default function App() {
         }
 
         fetchMovies();
+
+        return function () {
+            controller.abort();
+        }
     }, [query])
 
     return (

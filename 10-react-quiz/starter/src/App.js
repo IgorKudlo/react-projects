c81985dfdbfca04e74c6d1efaf27d653048fc9b1
igor.kudlo <igoreklida@gmail.com>
2023-11-20 12:40:04 +0300

    REACT-QUIZ: type status, start screen

--- 10-react-quiz/starter/src/App.js
+++ 10-react-quiz/starter/src/App.js
@@ -1,6 +1,9 @@
+import {useEffect, useReducer} from "react";
 import Header from "./Header";
 import Main from "./Main";
-import {useEffect, useReducer} from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
     questions: [],
@@ -18,7 +21,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+
+    const numQuestions = questions.length;
 
     useEffect(() => {
         fetch("http://localhost:8000/questions")
@@ -32,6 +37,9 @@ export default function App() {
             <Header />
 
             <Main>
+                {status === "loading" && <Loader />}
+                {status === "error" && <Error />}
+                {status === "ready" && <StartScreen numQuestions={numQuestions} />}
                 <p>1/15</p>
                 <p>Question?</p>
             </Main>

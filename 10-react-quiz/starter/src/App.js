26ac2b6acd8ff05a7a6e78b1ef2a343a43358130
igor.kudlo <igoreklida@gmail.com>
2023-11-20 12:31:34 +0300

    REACT-QUIZ: add json-server for fake api, create reducer

--- 10-react-quiz/starter/src/App.js
+++ 10-react-quiz/starter/src/App.js
@@ -1,7 +1,40 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+    questions: [],
+    status: "loading"
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataReceived":
+            return {...initialState, questions: action.payload, status: "ready"};
+        case "dataFailed":
+            return {...initialState, status: "error"};
+        default: return state;
+    }
+}
 
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/questions")
+            .then(res => res.json())
+            .then(data => dispatch({type: "dataReceived", payload: data }))
+            .catch(err => dispatch({type: "dataFailed"}))
+    }, []);
+
     return (
-        <DateCounter/>
+        <div className="app">
+            <Header />
+
+            <Main>
+                <p>1/15</p>
+                <p>Question?</p>
+            </Main>
+        </div>
     );
 }
